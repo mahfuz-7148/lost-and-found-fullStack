3ea7400f1a2b13a94f9bbfe64b4d82f79fd3b18d
mahfuz-7148 <mrahman7148@gmail.com>
2025-07-07 16:59:27 +0600

    updating verify token

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const jwt = require('jsonwebtoken')
 // });
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://lost-and-found-full-stack-mlo8.vercel.app'],
     credentials: true,
     optionSuccessStatus: 200,
 }

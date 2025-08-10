3dfe111decc80fe055644524774fe0de20565e69
mahfuz-7148 <mrahman7148@gmail.com>
2025-08-10 18:51:40 +0600

    updated deploy URL

--- server/index.js
+++ server/index.js
@@ -76,33 +76,33 @@ async function run() {
         const itemsCollection = client.db('lostFoundDB').collection('items')
         const recoveryCollection = client.db('lostFoundDB').collection('recoveryItems')
 
-        app.post('/jwt', async (req, res) => {
-            const email = req.body
-            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '365d',
-            })
-            res
-                .cookie('token', token, {
-                    httpOnly: true,
-                    secure: process.env.NODE_ENV === 'production',
-                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-                })
-                .send({ success: true })
-        })
-        // Logout
-        app.get('/logout', async (req, res) => {
-            try {
-                res
-                    .clearCookie('token', {
-                        maxAge: 0,
-                        secure: process.env.NODE_ENV === 'production',
-                        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-                    })
-                    .send({ success: true })
-            } catch (err) {
-                res.status(500).send(err)
-            }
-        })
+        // app.post('/jwt', async (req, res) => {
+        //     const email = req.body
+        //     const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        //         expiresIn: '365d',
+        //     })
+        //     res
+        //         .cookie('token', token, {
+        //             httpOnly: true,
+        //             secure: process.env.NODE_ENV === 'production',
+        //             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        //         })
+        //         .send({ success: true })
+        // })
+        // // Logout
+        // app.get('/logout', async (req, res) => {
+        //     try {
+        //         res
+        //             .clearCookie('token', {
+        //                 maxAge: 0,
+        //                 secure: process.env.NODE_ENV === 'production',
+        //                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        //             })
+        //             .send({ success: true })
+        //     } catch (err) {
+        //         res.status(500).send(err)
+        //     }
+        // })
 
         app.get("/allItem", async (req, res) => {
             try {
